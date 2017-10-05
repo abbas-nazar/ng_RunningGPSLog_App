214d2a495809776969ec3af2bb64f6a057350bf9
Abbas Nazar <abbasnazar.970@gmail.com>
2017-10-05 00:14:48 -0700

    congig_done

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { MapComponent } from './map/map.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActivityListComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/activities.ts
@@ -0,0 +1,13 @@
+import { IActivity } from './activity.model';
+
+export const saved_ACTIVITIES: IActivity [] =
+[
+{
+    'id' : 1,
+    'name': 'main Bike trails',
+    'date': new Date('06/01/2017'),
+    'distance': 6.2,
+    'comments': 'Nice day',
+    'gpxData': '../../assets/gpx'
+}
+]
