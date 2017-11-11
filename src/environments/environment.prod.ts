36550a660f3cd8d6de043d7c7a9df463355076d2
Abbas Nazar <abbasnazar.970@gmail.com>
2017-11-11 13:52:08 +0500

    environment_prod_fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  MAPBOX_API_KEY:'pk.eyJ1IjoiYWJiYXM5NzAiLCJhIjoiY2o1cDQ2a3l6MDZ1aTJxcDkyY2tmbThlayJ9.AclJWLBVqXenWYSdh_xADA'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  MAPBOX_API_KEY :'pk.eyJ1IjoiYWJiYXM5NzAiLCJhIjoiY2o1cDQ2a3l6MDZ1aTJxcDkyY2tmbThlayJ9.AclJWLBVqXenWYSdh_xADA',
+  MAPBOX_API_KEY:'pk.eyJ1IjoiYWJiYXM5NzAiLCJhIjoiY2o1cDQ2a3l6MDZ1aTJxcDkyY2tmbThlayJ9.AclJWLBVqXenWYSdh_xADA'
 };
